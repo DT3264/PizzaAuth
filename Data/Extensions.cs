1b010e6d7dc297195394d5cf4a15216998bd116f
DT3264 <cernadaniel32@gmail.com>
2022-03-31 22:00:58 -0600

    first commit

---
+++ Data/Extensions.cs
@@ -0,0 +1,19 @@
+namespace aspnetcore_react_auth.Data;
+
+public static class Extensions
+{
+    public static void CreateDbIfNotExists(this IHost host)
+    {
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                if (context.Database.EnsureCreated())
+                {
+                    DbInitializer.Initialize(context);
+                }
+            }
+        }
+    }
+}
